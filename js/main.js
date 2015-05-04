83450a73510357540c5964125eed423cc690379d
Michael Trilford <trilfordm@gmail.com>
2015-05-04 23:31:30 +1200

    Added the JS and HTML file

---
+++ js/main.js
@@ -0,0 +1,2 @@
+/* 3 Column */
+$("body").append('<div class="grid"><div class="wrapper"><div class="columnWrap"><div class="column"></div><div class="column"></div><div class="column"></div></div></div></div>')
