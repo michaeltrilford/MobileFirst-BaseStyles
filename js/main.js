ba83e146f55f54735d9fd1df0e2fae89f1d0cf00
Michael Trilford <trilfordm@gmail.com>
2015-07-02 18:08:38 +1200

    Update Base CSS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-/* 1. OVERLAY GRID
+// Remember to delete comments / minify this document. Remove what you don't want.
+
+/* A. OVERLAY GRID
 ------------------------------------------------------------------- */
 $(".grid").append('<div class="wrapper"><div class="columnWrap"></div></div>');
 var str = '';
@@ -7,78 +9,144 @@ for(var cnt=0; cnt < 6; cnt++){
 }
 $(".columnWrap").append(str);
 
-/* 2. TRANSFORMICONS SIMPLIFIED
+
+/* B. TRANSFORMICONS SIMPLIFIED
 ------------------------------------------------------------------- */
 $("a.tIcons").click(function(){
   $(this).toggleClass('toggle');
   return false;
 });
 
-/* 3. PROGRESSIVELY ENHANCEMENTS ADDED AT 500px - http://codepen.io/trilm/pen/dPLGEL
-------------------------------------------------------------------- */
-function mediaQuery () {
-  
-  // Not needed if using the Modernizr Option
-  var mq = window.matchMedia("(min-width: 500px)");
-  
-  // 1. ADD BASIC ELEMENTS - SMALL ITEMS OF CODE
-  // MUST ADD: "refresh" & "desktop" classes to added items
-  ////////////////////////////////////////////////////////////////////
-  function basicDesktopView () {
-    var elementRefresh = function () { $(".refresh").remove(); }
-    elementRefresh(); 
-    $(".element").prepend("<h2 class='refresh desktop'>Progressively Enhanced Experience</h2>");
-  }
-  
-  // 2. ADD ADVANCED ELEMENTS - LARGE ITEMS OF CODE
-  ////////////////////////////////////////////////////////////////////
-  function advancedDesktopView () {
-    $(".element").append("<aside class='desktop'><h4><mark>Ajax Example:</mark></h4><p>I'm a feature that's loaded when the browser is above 500px.</p></aside>");
-    // $('#ajaxElement').load('path.html');
-    // $("#ajaxElement").load('path.html');
-  }
-  
-  // 3. ON RESIZE FUNCTION
-  ////////////////////////////////////////////////////////////////////
-  /* A. Check Browser Width */
-  /* B. Add Basic Desktop Elements */
-  ////////////////////////////////////////////////////////////////////
-  function injectElementResize () {
-     if (mq.matches) {
-      basicDesktopView();
-    } else {} 
-    // Modernizr Option - Conflicts with Codepen I believe
-		/* if (Modernizr.mq('(min-width: 500px)')) {
-      basicDesktopView(); 
-    } else {} */  
-  }
-    
-  // 4. ON LOAD FUNCTION
-  ////////////////////////////////////////////////////////////////////
-  /* A. Check Browser Width */
-  /* B. Add Basic Desktop Elements */
-  /* C. Add Advanced Desktop Features */
-  ////////////////////////////////////////////////////////////////////
-  function injectElementLoad () {
-    if (mq.matches) {
-      basicDesktopView();
-      advancedDesktopView();
-    } else {}
-    // Modernizr Option - Conflicts with Codepen I believe
-    /* if (Modernizr.mq('(min-width: 500px)')) {
-      basicDesktopView();
-      advancedDesktopView();
-    } else {} */  
-  }
 
-  // 5. INITIALISE: ON LOAD
-  ////////////////////////////////////////////////////////////////////
-  injectElementLoad();
+/* C. SCROLL NAV
+------------------------------------------------------------------- */
+$(window).scroll(function() {
+  if( $(this).scrollTop() > 81 ) {
+    $(".navScroll").addClass("scrollEnabled");
+  }
+  else {
+    $(".navScroll").removeClass("scrollEnabled");
+  }
+});
+
+
+/* D. No SVG support
+------------------------------------------------------------------- */
+$(".no-svg svg").remove();
+// Example Code to add alternative image
+$(".no-svg .logo-wrap a").append("<img src='http://placehold.it/150x50'>");
+
+
+/*---------------------------------------------------
+  E. INJECT UI FOR ALT VIEWPORTS - http://codepen.io/trilm/pen/xGLVGM
+---------------------------------------------------*/
+function injectQuery () {
   
-  // 6. INITIALISE: ON RESIZE
-  ////////////////////////////////////////////////////////////////////
-  window.onresize = function() { injectElementResize(); };
+  
+  /*---------------------------------------------------
+  #1 - ITEMS FOR INJECTING
+  ---------------------------------------------------*/
+
+  
+      // A: ADD SKELETON MARKUP
+  
+            // Empty Base Elements
+            function baseInject () {
+              // $("main").append("<div class='inject'></div>");
+            }    
+            // Add Empty Base Elements
+            baseInject();
+  
+  
+      // B: INSERT BASIC ELEMENTS ( SMALL FEATURES )       
+
+      // Basic elements need the class="refresh"
+
+            // Basic Smartphone
+            function basicSmartphone () {
+     
+            }
+            // Basic Tablet
+            function basicTablet () {
+              
+            }
+            // Basic Desktop
+            function basicDesktop () {
+              $('#element').append("<h1 class='refresh'>Hello</h1>");
+
+            }
+     
+      // C: INSERT ADVANCED ELEMENTS ( LARGER FEATURES - AJAX )
+          
+            // AJAX ITEMS
+            function advancedDesktop () {
+              $('#ajax').load('path.html');
+            } 
+  
+  /*---------------------------------------------------
+  #2 - FIND BREAKPOINTS
+  // Code from: https://www.lullabot.com/blog/article/importing-css-breakpoints-javascript
+  ---------------------------------------------------*/
+  var breakpoint = {};
+  breakpoint.refreshValue = function () {
+    this.value = window.getComputedStyle(
+      document.querySelector('body'), ':before'
+    ).getPropertyValue('content').replace(/['"]+/g, '')
+  };
+  
+  /*---------------------------------------------------
+  #3 - ON RESIZE: ADD BASIC ELEMENTS
+  ---------------------------------------------------*/
+  $(window).resize(function(){
+    
+    var elementRefresh = function () { $(".refresh").remove(); }
+    // Discover Breakpoint
+    breakpoint.refreshValue();
+    
+    // Needed for appended items
+    elementRefresh();
+    
+    // Example of how to use - Run Certain functions by adding in a class to run the function.
+    /*    if ((breakpoint.value == 'smartphone') || (breakpoint.value == 'tablet')) {
+          basicSmartphone();
+          $("header").addClass('scrollHeader');
+        } else {
+          $(".scrollHeader").removeClass('scrollHeader');
+        }*/
+
+    // Smartphone
+    if (breakpoint.value == 'smartphone') {
+      basicSmartphone();
+    } else {}
+    
+    // Tablet
+    if (breakpoint.value == 'tablet') {
+      basicTablet();
+    } else {}
+
+    // Desktop
+    if (breakpoint.value == 'desktop') {
+      basicDesktop();
+    } else {} 
+    
+  }).resize();
+
+
+  /*---------------------------------------------------
+  #4 - ON LOAD: ADD AJAX ITEMS
+  ---------------------------------------------------*/  
+  function injectLoad () {
+    
+    // Desktop
+    if (breakpoint.value == 'desktop') {
+      advancedDesktop();
+    } else {}
+    
+  }
+  // INITIALISE: ON LOAD
+  injectLoad(); 
+  
 }
 
 // 7. RUN FUNCTION
-mediaQuery();
+injectQuery();
