35243c3f38cb0a9a6e85957316759a69d3d02c4a
michaeltrilford <trilfordm@gmail.com>
2017-07-08 18:10:05 +1000

    Added navigation component

--- js/main.js
+++ js/main.js
@@ -1,6 +1,69 @@
-// Remember to delete comments / minify this document. Remove what you don't want.
-$(".preSecond").hide();
+/* A. TRANSFORMICONS SIMPLIFIED
+------------------------------------------------------------------- */
+$("a.tIcons").click(function(){
+  $(this).toggleClass('toggle');
+  return false;
+});
 
+
+/* B. SCROLL NAV
+------------------------------------------------------------------- */
+$(window).scroll(function() {
+  if( $(this).scrollTop() > 81 ) {
+    $(".navScroll").addClass("scrollEnabled");
+  }
+  else {
+    $(".navScroll").removeClass("scrollEnabled");
+  }
+});
+
+/* C. Baseline Grid
+------------------------------------------------------------------- */
+$('.baselineGridSetup').append('<div class="baselineGrid"></div>');
+var baselineGrid = $(".baselineGrid");
+var count = 24;
+for (var i=0; i<count; i++) {
+   baselineGrid.append("<div>");
+}
+
+/* D. MobileFirst Nav
+------------------------------------------------------------------- */
+$('.mui-mfn-menu-toggle').click(function(){
+  $(this).toggleClass('toggle-on');
+  $('body').toggleClass('mui-mfn-fixed-background');
+  $('.mui-mfn-nav').toggleClass('mui-mfn-menu-open');
+  $('.mui-mfn-nav li a').toggleClass('mui-mfn-menu-fadeIn');
+  return false
+});
+
+/* E. Priority Nav
+------------------------------------------------------------------- */
+// Append 'More...' button
+$(".mui-pn-inner").append("<a id='mui-pn-reveal' class='mui-pn-switch' href='#'><span>More&nbsp;&#8230;</span><span style='display: none;'>&#8230;&nbsp;Less</span></a>");
+// Toggle Text from 'More..' to '...Less'
+$(document).on('click', '.mui-pn-switch', function () {
+  $(this).children().toggle();
+  return false
+});
+// Mobile: Show menu items
+$("#mui-pn-mobile-menu").click(function(){
+  $(this).toggleClass("mui-pn-active");
+  $(this).next().slideToggle();
+});
+// Access Data-Nav-Count Value
+var navCount = $(".mui-pn").attr('data-nav-count');
+// Access children in navigation / Pass in navCount from HTML & CSS
+$(".mui-pn-inner").children().slice(0,navCount).css( "display", "inline-block" );
+// Reveal Hidden Navigation Items
+$(document).on('click', '#mui-pn-reveal', function () {
+  $(".mui-pn-inner").toggleClass("mui-pn-reveal");
+  return false
+});
+
+
+/* F. Tabs for code snippets
+------------------------------------------------------------------- */
+$(".preSecond").hide();
 $(".menu button.first").click(function(){
   $(".preSecond").hide();
   $(".preFirst").show();
@@ -12,169 +75,6 @@ $(".menu button.second").click(function(){
   $(".preSecond").show();
   $("button.first").removeClass("active");
   $("button.second").addClass("active");
-});
-
-/* B. TRANSFORMICONS SIMPLIFIED
-------------------------------------------------------------------- */
-$("a.tIcons").click(function(){
-  $(this).toggleClass('toggle');
-  return false;
-});
-
-
-/* C. SCROLL NAV
-------------------------------------------------------------------- */
-$(window).scroll(function() {
-  if( $(this).scrollTop() > 81 ) {
-    $(".navScroll").addClass("scrollEnabled");
-  }
-  else {
-    $(".navScroll").removeClass("scrollEnabled");
-  }
-});
-
-
-/* D. No SVG support
-------------------------------------------------------------------- */
-$(".no-svg svg").remove();
-// Example Code to add alternative image
-$(".no-svg .logo-wrap a").append("<img src='http://placehold.it/150x50'>");
-
-
-/*---------------------------------------------------
-  E. INJECT UI FOR ALT VIEWPORTS - http://codepen.io/trilm/pen/xGLVGM
----------------------------------------------------*/
-function injectQuery () {
-  
-  
-  /*---------------------------------------------------
-  #1 - ITEMS FOR INJECTING
-  ---------------------------------------------------*/
-
-  
-      // A: ADD SKELETON MARKUP
-  
-            // Empty Base Elements
-            function baseInject () {
-              // $("main").append("<div class='inject'></div>");
-            }    
-            // Add Empty Base Elements
-            baseInject();
-  
-  
-      // B: INSERT BASIC ELEMENTS ( SMALL FEATURES )       
-
-      // Basic elements need the class="refresh"
-
-            // Basic Smartphone
-            function basicSmartphone () {
-     
-            }
-            // Basic Tablet
-            function basicTablet () {
-              
-            }
-            // Basic Desktop
-            function basicDesktop () {
-              $('#element').append("<h1 class='refresh'>Hello</h1>");
-
-            }
-     
-      // C: INSERT ADVANCED ELEMENTS ( LARGER FEATURES - AJAX )
-          
-            // AJAX ITEMS
-            function advancedDesktop () {
-              $('#ajax').load('path.html');
-            } 
-  
-  /*---------------------------------------------------
-  #2 - FIND BREAKPOINTS
-  // Code from: https://www.lullabot.com/blog/article/importing-css-breakpoints-javascript
-  ---------------------------------------------------*/
-  var breakpoint = {};
-  breakpoint.refreshValue = function () {
-    this.value = window.getComputedStyle(
-      document.querySelector('body'), ':before'
-    ).getPropertyValue('content').replace(/['"]+/g, '')
-  };
-  
-  /*---------------------------------------------------
-  #3 - ON RESIZE: ADD BASIC ELEMENTS
-  ---------------------------------------------------*/
-  $(window).resize(function(){
-    
-    var elementRefresh = function () { $(".refresh").remove(); }
-    // Discover Breakpoint
-    breakpoint.refreshValue();
-    
-    // Needed for appended items
-    elementRefresh();
-    
-    // Example of how to use - Run Certain functions by adding in a class to run the function.
-    /*    if ((breakpoint.value == 'smartphone') || (breakpoint.value == 'tablet')) {
-          basicSmartphone();
-          $("header").addClass('scrollHeader');
-        } else {
-          $(".scrollHeader").removeClass('scrollHeader');
-        }*/
-
-    // Smartphone
-    if (breakpoint.value == 'smartphone') {
-      basicSmartphone();
-    } else {}
-    
-    // Tablet
-    if (breakpoint.value == 'tablet') {
-      basicTablet();
-    } else {}
-
-    // Desktop
-    if (breakpoint.value == 'desktop') {
-      basicDesktop();
-    } else {} 
-    
-  }).resize();
-
-
-  /*---------------------------------------------------
-  #4 - ON LOAD: ADD AJAX ITEMS
-  ---------------------------------------------------*/  
-  function injectLoad () {
-    
-    // Desktop
-    if (breakpoint.value == 'desktop') {
-      advancedDesktop();
-    } else {}
-    
-  }
-  // INITIALISE: ON LOAD
-  injectLoad(); 
-  
-}
-
-// 7. RUN FUNCTION
-injectQuery();
-
-
-
-
-// E. Baseline Grid
-$('.baselineGridSetup').append('<div class="baselineGrid"></div>');
-var baselineGrid = $(".baselineGrid");
-var count = 24;
-for (var i=0; i<count; i++) {
-   baselineGrid.append("<div>");
-}
-
-
-// F. MobileFirst Nav
-// Reveal Menu
-$('.mui-mfn-menu-toggle').click(function(){
-  $(this).toggleClass('toggle-on');
-  $('body').toggleClass('mui-mfn-fixed-background');
-  $('.mui-mfn-nav').toggleClass('mui-mfn-menu-open');
-  $('.mui-mfn-nav li a').toggleClass('mui-mfn-menu-fadeIn');
-  return false
 });
 
 
