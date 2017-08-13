8072987c07fa5ecdfa236109f8b774b12ec9616b
michaeltrilford <trilfordm@gmail.com>
2017-08-13 14:56:08 +1000

    Removed JQuery Dependencies

--- js/main.js
+++ js/main.js
@@ -1,81 +1,79 @@
 /* A. TRANSFORMICONS SIMPLIFIED
 ------------------------------------------------------------------- */
-$("a.tIcons").click(function(){
-  $(this).toggleClass('toggle');
-  return false;
+u("a.tIcons").on('click', function(e) {
+  e.preventDefault();
+  u(this).toggleClass('toggle');
 });
 
-
-/* B. SCROLL NAV
+/* B. Baseline Grid
 ------------------------------------------------------------------- */
-$(window).scroll(function() {
-  if( $(this).scrollTop() > 81 ) {
-    $(".navScroll").addClass("scrollEnabled");
-  }
-  else {
-    $(".navScroll").removeClass("scrollEnabled");
-  }
-});
-
-/* C. Baseline Grid
-------------------------------------------------------------------- */
-$('.baselineGridSetup').append('<div class="baselineGrid"></div>');
-var baselineGrid = $(".baselineGrid");
+u('.mui-baselineGridSetup').append('<div class="mui-baselineGrid"></div>');
+var baselineGrid = u(".mui-baselineGrid");
 var count = 24;
 for (var i=0; i<count; i++) {
    baselineGrid.append("<div>");
 }
 
-/* D. MobileFirst Nav
+/* C. MobileFirst Nav
 ------------------------------------------------------------------- */
-$('#mui-mfn-menu-toggle').click(function(){
-  $(this).toggleClass('toggle-on');
-  $('body').toggleClass('mui-mfn-fixed-background');
-  $('.mui-mfn-nav').toggleClass('mui-mfn-menu-open');
-  $('.mui-mfn-nav li a').toggleClass('mui-mfn-menu-fadeIn');
-  return false
+u('#mui-mfn-menu-toggle').on('click', function(e) {
+  e.preventDefault();
+  u('body').toggleClass('mui-mfn-fixed-background');
+  u('.mui-mfn-nav').toggleClass('mui-mfn-menu-open');
+  u('.mui-mfn-nav li a').toggleClass('mui-mfn-menu-fadeIn');
 });
+
+
+/* D. Tabs for code snippets
+------------------------------------------------------------------- */
+u("pre:first-child").addClass('mui-show');
+u("pre").addClass('mui-hide');
+
+// First
+u(".menu button:nth-of-type(1)").on('click', function(e) {
+  e.preventDefault();
+  u(this).parent().children().removeClass("active");
+  u(this).addClass("active");
+  u(this).parent().siblings(".codeSnippet").children("pre").removeClass('mui-show').addClass('mui-hide');
+  u(this).parent().siblings(".codeSnippet").children("pre:nth-of-type(1)").removeClass('mui-hide').addClass('mui-show');
+});
+// Second
+u(".menu button:nth-of-type(2)").on('click', function(e) {
+  e.preventDefault();
+  u(this).parent().children().removeClass("active");
+  u(this).addClass("active");
+  u(this).parent().siblings(".codeSnippet").children("pre").removeClass('mui-show').addClass('mui-hide');
+  u(this).parent().siblings(".codeSnippet").children("pre:nth-of-type(2)").removeClass('mui-hide').addClass('mui-show');
+});
+
 
 /* E. Priority Nav
 ------------------------------------------------------------------- */
 // Append 'More...' button
-$(".mui-pn-inner").append("<a id='mui-pn-reveal' class='mui-pn-switch' href='#'><span>More&nbsp;&#8230;</span><span style='display: none;'>&#8230;&nbsp;Less</span></a>");
+u(".mui-pn-inner").append("<a id='mui-pn-reveal' class='mui-pn-switch' href='#'><span>More&nbsp;&#8230;</span><span style='display: none;'>&#8230;&nbsp;Less</span></a>");
 // Toggle Text from 'More..' to '...Less'
-$(document).on('click', '.mui-pn-switch', function () {
-  $(this).children().toggle();
-  return false
+u(document).on('click', '.mui-pn-switch', function (e) {
+  e.preventDefault();
+  u(this).children().toggle();
 });
 // Mobile: Show menu items
-$("#mui-pn-mobile-menu").click(function(){
-  $(this).toggleClass("mui-pn-active");
-  $(this).next().slideToggle();
+u("#mui-pn-mobile-menu").on('click', function(e) {
+  e.preventDefault();
+  u(this).toggleClass("mui-pn-active");
 });
 // Access Data-Nav-Count Value
-var navCount = $(".mui-pn").attr('data-nav-count');
+var navCount = u(".mui-pn").attr('data-nav-count');
 // Access children in navigation / Pass in navCount from HTML & CSS
-$(".mui-pn-inner").children().slice(0,navCount).css( "display", "inline-block" );
+var myElements = u('.mui-pn-inner').children();
+u(myElements.nodes.slice(0, navCount)).addClass('inline-block');
 // Reveal Hidden Navigation Items
-$(document).on('click', '#mui-pn-reveal', function () {
-  $(".mui-pn-inner").toggleClass("mui-pn-reveal");
-  return false
+u('#mui-pn-reveal').on('click', function(e) {
+  e.preventDefault();
+  u(".mui-pn-inner").toggleClass("mui-pn-reveal");
 });
 
 
-/* F. Tabs for code snippets
-------------------------------------------------------------------- */
-$(".preSecond").hide();
-$(".menu button.first").click(function(){
-  $(".preSecond").hide();
-  $(".preFirst").show();
-  $("button.second").removeClass("active");
-  $("button.first").addClass("active");
-});
-$(".menu button.second").click(function(){
-  $(".preFirst").hide();
-  $(".preSecond").show();
-  $("button.first").removeClass("active");
-  $("button.second").addClass("active");
-});
+
 
 
 
